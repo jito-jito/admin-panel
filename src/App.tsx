0cc700b60670d658c80672a8034a370564ec08b8
jito <sergio.sergio.98@hotmail.com>
2022-03-22 14:33:15 -0300

    add environment to work with MUI, SASS, ReactRouter v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import { Routes, Route, Link } from "react-router-dom";
+import './styles/app.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Button variant="contained" className="button">Hello World in main route</Button>} />
+        <Route path="home" element={<Button variant="contained" className="button">Hello World in home route</Button>} />
+      </Routes>
     </div>
   );
 }
